4d4cdc4fb68ec356d9b27459550736101ef25ca7
ransomsiyeofori-dotcom <ransomsiyeofori@gmail.com>
2026-06-21 20:10:41 +0100

    Update signUp.js

--- signUp.js
+++ signUp.js
@@ -3,11 +3,24 @@ const nav = document.getElementById("nav");
 
 let open = false;
 
-menu.addEventListener("click", () => {
+function siderbar() {
   open = !open;
+
   nav.classList.toggle("active");
-  // change icon
   menu.textContent = open ? "close" : "menu";
+}
+
+// Close menu when user taps outside
+document.addEventListener("click", (e) => {
+  if (
+    open &&
+    !nav.contains(e.target) &&
+    !menu.contains(e.target)
+  ) {
+    nav.classList.remove("active");
+    menu.textContent = "menu";
+    open = false;
+  }
 });
 
 // SEARCH
